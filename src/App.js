c222d8fd962f56b931b9db95cc1bdb28cee228ed
vaibhav-gosavi <119005090+vaibhav-gosavi@users.noreply.github.com>
2024-08-26 18:42:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import NutritionMeter from "./NutritionMeter"; 
+  
+function App() { 
+  return ( 
+    <div className="bg-gray-100 min-h-screen"> 
+      <NutritionMeter /> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
